374413c4fbc2a40f1b935b70e15d69bd690b04c4
yongwohwang <75257106+yongwohwang@users.noreply.github.com>
2021-06-08 18:30:23 +0700

    review

--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
-
+import "./Movie.css";
 function Movie({ id, year, title, summary, poster }) {
-  return <h5>{title}</h5>;
+  return (
+    <div class="movie">
+      <img src={poster} alt={title}></img>
+      <div class="movie__data">
+        <h3 class="movie__title">{title}</h3>
+        <h5 class="movie__year">{year}</h5>
+        <p class="movie__summary">{summary}</p>
+      </div>
+    </div>
+  );
 }
 
 Movie.propTypes = {
